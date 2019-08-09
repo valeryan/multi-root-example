4408cd83fa8d4580d7299f3b215e311f5af4dd96
Samuel Hilson <samrhilson@gmail.com>
2019-08-09 10:39:25 -0500

    Setup Test Bed

---
+++ root2/src/Example.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Root 2 Example
+ *
+ * @package Root2
+ * @author  Samuel Hilson <samrhilson@gmail.com>
+ * @license MIT
+ */
+
+namespace Root2;
+
+class Example {
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		//
+	}
+}

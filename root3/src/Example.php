eb4cfff89c5c6c8bb43c2801192e7c3db29ddfd4
Samuel Hilson <samrhilson@gmail.com>
2020-07-05 17:30:46 -0500

    Update with 3rd root just for more testing

---
+++ root3/src/Example.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Root 3 Example
+ *
+ * @package Root3
+ * @author  Samuel Hilson <samrhilson@gmail.com>
+ * @license MIT
+ */
+
+namespace Root2;
+
+class Example {
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		//
+	}
+}
